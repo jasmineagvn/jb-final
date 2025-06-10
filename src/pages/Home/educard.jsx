d36486fe196864e65d8f183f6bf30d318f0292db
Bevinda Vivian <19623228@std.stei.itb.ac.id>
2025-06-11 01:33:48 +0700

    fix: homepage daftar button

--- src/pages/Home/educard.jsx
+++ src/pages/Home/educard.jsx
@@ -1,14 +1,24 @@
+import { Link } from "react-router-dom";
 
-const Card = ({ icon, title, description, button }) => (
+const Card = ({ icon, title, description, button, link }) => (
   <div className="rounded-xl p-6 dark:bg-[#3C3F41] shadow-2xl gap-5 flex flex-col items-center hover:bg-[rgba(69,179,157,0.2)]">
-    <div className="text-xl text-cyan-600  mb-4">{icon}</div>
+    <div className="text-xl text-cyan-600 mb-4">{icon}</div>
     <h2 className="text-xl dark:text-white font-semibold mb-2">{title}</h2>
     <p className="text-gray-600 dark:text-gray-300 text-sm mb-6 flex-1 text-center">
       {description}
     </p>
-    <button className="bg-[#EC901D] text-white px-6 py-2 rounded-full hover:bg-orange-600 transition cursor-pointer">
-      {button}
-    </button>
+    {link ? (
+      <Link 
+        to={link}
+        className="bg-[#EC901D] text-white px-6 py-2 rounded-full hover:bg-orange-600 transition cursor-pointer inline-block text-center"
+      >
+        {button}
+      </Link>
+    ) : (
+      <button className="bg-[#EC901D] text-white px-6 py-2 rounded-full hover:bg-orange-600 transition cursor-pointer">
+        {button}
+      </button>
+    )}
   </div>
 );
 
@@ -20,12 +30,14 @@ const EducationCards = () => {
         title="Menjadi Siswa"
         description="Dapatkan pendidikan berkualitas untuk masa depan yang lebih cerah. Bergabunglah bersama kami dan nikmati fasilitas belajar lengkap,program pengembangan siswa, serta lingkungan belajar yang mendukung"
         button="Daftar Sebagai Siswa"
+        link="/syaratdaftarsiswa"
       />
       <Card
         icon={<img src="/icons/relawan.png" width={80} height={80}></img>}
         title="Menjadi Relawan"
         description="Bergabunglah sebagai pengajar, mentor, atau tenaga relawan untuk membantu anak-anak belajar dan berkembang. Anda bisa berbagi ilmu, pengalaman, atau keterampilan dengan mereka."
         button="Daftar Sebagai Relawan"
+        link="/daftarrelawan"
       />
       <Card
         icon={<img src="/icons/donation.png" width={80} height={80}></img>}
